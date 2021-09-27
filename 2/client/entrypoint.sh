bf63d5a877767dab09ef3a7c46255f73147b56ea
lei.tian <whereismoney@qq.com>
2021-09-27 18:12:27 +0800

    修复client镜像无法正常启动问题

--- 2/client/entrypoint.sh
+++ 2/client/entrypoint.sh
@@ -3,9 +3,28 @@
 HFISH_DIR="/opt/hfish/client"
 cd $HFISH_DIR
 
-CLIENT_URL=""
-curl -k "$CLIENT_URL" -o client
+echo "download client"
+curl -k "$CLIENT_URL" -o client.tgz
+echo "download client ok!"
+
+old_id=""
+if test -f "config.ini"
+then
+  old_id=`grep "client_id" config.ini`
+  echo "old client_id: $old_id"
+fi
+
+tar -xvzf client.tgz
+echo "decompress client ok!"
+rm -f client.tgz
+
+if [ -n "$old_id" ]
+then
+  echo $old_id >> "config.ini"
+fi
+
 chmod a+x client
 
-nohup ./client >/dev/null 2>&1 &
-ll 2>&1 &
+echo "Install client finish!"
+
+./client
